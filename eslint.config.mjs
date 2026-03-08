ba7330fb0b3999656d6634833968e6d55bbe66a6
Helene Syre <syrehelene@gmail.com>
2026-03-08 21:50:09 +0100

    Added ESLint and Prettier configs and format files
    Installed and configured ESLint and Prettier. Basic global handling is fixed. Used Prettier to fix formats across the project.

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        module: true,
+        require: true,
+        process: true,
+      },
+    },
+  },
+]);
